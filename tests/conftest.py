26a7f78fe4448f6d2d746565a01c5a20b85668c9
Stelios Kiliaris <stylianosk7@gmail.com>
2025-12-29 13:34:30 +0200

    Create conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,8 @@
+import sys
+import os
+
+# Path to project root (directory containing src/)
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+if PROJECT_ROOT not in sys.path:
+    sys.path.insert(0, PROJECT_ROOT)
